1da7907dcbd5381239d3be0615390d18df422fc1
pcardenal <pcardenalra@gmail.com>
2024-07-26 23:24:35 +0200

    Add spell parser

---
+++ src/Spells.py
@@ -0,0 +1,47 @@
+import re
+class Spell():
+    def __init__(self) -> None:
+        pass
+
+    def read(self, fileName : str ) -> list[str]:
+        with open(fileName, 'r') as inputFile:
+            spellLines = inputFile.readlines()
+        # Remove empty lines and \n characters
+        return [re.sub(r"\*\*[\w\s]+?:\*\*\s*", '', line.replace('\n', '')) for line in spellLines if line != '\n']
+
+    def parse(self, spellLines : list[str]) -> dict[str,str]:
+        spellDict = {}
+        spellDict['Name'] = self.cleanName(spellLines[0])
+        spellDict['Level'], spellDict['School'] = self.cleanLevel(spellLines[1])
+        spellDict['Time'] = spellLines[2]
+        spellDict['Range'] = spellLines[3]
+        spellDict['Components'] = spellLines[4]
+        spellDict['Duration'] = spellLines[5]
+        spellDict['Description'] = spellLines[6]
+        try:
+            spellDict['Higher'] = spellLines[7]
+        except:
+            spellDict['Higher'] = ''
+        return spellDict
+
+    @staticmethod
+    def cleanName(spellName: str) -> str:
+        return re.sub(r'^#+\s', '', spellName)
+    
+
+    @staticmethod
+    def cleanLevel(spellLevel: str) -> tuple[str, str]:
+        levelRegex = re.compile(r'\*(\d)\w\w-level\s([\w]+)\*$').search(spellLevel)
+        if levelRegex:
+            return levelRegex.group(1), levelRegex.group(2).capitalize()
+        
+        cantipRegex = re.compile(r'\*([\w]+)\scantrip\*$').search(spellLevel)
+        if cantipRegex:
+            return '0', cantipRegex.group(1)
+        raise ValueError('Spell level and school not found')
+
+if __name__ == '__main__':
+    s = Spell()
+    spelllines = s.read('/data/DND.SRD.Wiki/Spells/Guidance.md')
+    print(spelllines)
+    print(s.parse(spelllines))
